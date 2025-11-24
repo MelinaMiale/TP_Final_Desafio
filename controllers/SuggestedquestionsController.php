dc637285c85653244c029ffb5eb2920e36d77f8e
MelinaMiale <melina.miale@gmail.com>
2025-11-23 22:14:29 -0300

    se agrega logica para que los jugadores puedan sugerir preguntas con sus respectivas respuestas. tambien se agrega nueva vista para notificar sugerencia. sigo normalizando rutas

--- controllers/SuggestedquestionsController.php
+++ controllers/SuggestedquestionsController.php
@@ -5,13 +5,32 @@ class SuggestedquestionsController {
     private $model;
     private $renderer;
 
+    private $categoryModel;
+
     public function __construct($model, $renderer) {
         $this->model = $model;
         $this->renderer = $renderer;
+        $this->categoryModel = new CategoryModel($this->model->getConnection());
     }
 
     public function displayForm() {
-        $this->renderer->render("suggestQuestion");
+        $categories = $this->categoryModel->getAllCategories();
+        $this->renderer->render("suggestQuestion", [
+            'categories' => $categories
+        ]);
     }
 
+    public function suggestQuestion() {
+        $suggestedQuestion = [
+            'question' => $_POST['question'],
+            'option_a' => $_POST['option_a'],
+            'option_b' => $_POST['option_b'],
+            'option_c' => $_POST['option_c'],
+            'option_d' => $_POST['option_d'],
+            'categoryId' => $_POST['categoryId'],
+            'correct_answer' => $_POST['correct_answer']
+        ];
+        $this->model->saveSuggestData($suggestedQuestion);
+        $this->renderer->render("suggestionSubmitted");
+    }
 }
